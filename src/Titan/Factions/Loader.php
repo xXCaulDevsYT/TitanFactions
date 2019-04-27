2e87705c792a2003d733e1b7dbfff64c9662e1e9
Havana <34898438+xXCaulDevsYT@users.noreply.github.com>
2019-04-27 16:03:01 -0500

    Create Loader.php

---
+++ src/Titan/Factions/Loader.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Titan\Factions\Loader;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+
+class Loader extends PluginBase implements Listener {
+}
